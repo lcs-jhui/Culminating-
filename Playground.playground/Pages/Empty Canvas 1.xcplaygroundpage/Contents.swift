3161cf39fb3a2d6dc9a7d59c9e5a4de8fee1f27c
jhui <jhui@lcs.on.ca>
2022-05-14 10:55:56 -0400

    Finished version, removed the little dot that was poking out

--- Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
@@ -42,119 +42,127 @@ canvas.highPerformance = true
 // Create a turtle that will draw upon the canvas
 let turtle = Tortoise(drawingUpon: canvas)
 
+//create scale
+let scale  = 20
+
 //set pen colour
 turtle.setPenColor(to: .black)
 
 //let scale = 20
 
 //set turtle starting position
+turtle.penUp()
 turtle.setPosition(to: Point(x: 0, y: 0))
 
 //turtle fill a square function
 func fillSquare() {
+        
     for _ in 1 ... 10 {
-        turtle.penDown()
         turtle.setHeading(to: 90)
-        turtle.forward(steps: 20)
+        turtle.penDown()
+        turtle.forward(steps: 1 * scale)
         turtle.right(by: 90)
         turtle.forward(steps: 1)
         turtle.right(by: 90)
-        turtle.forward(steps: 20)
+        turtle.forward(steps: 1 * scale)
         turtle.left(by: 90)
+        turtle.penUp()
         turtle.forward(steps: 1)
         turtle.left(by: 90)
         turtle.penUp()
     }
+   
     turtle.right(by: 90)
-   //turtle.drawSelf()
+    //turtle.drawSelf()
 }
+
 
 func drawTessellation() {
     //draw row 1
     fillSquare()
     fillSquare()
     fillSquare()
-    
+
     //draw row 2
     turtle.left(by: 180)
-    turtle.forward(steps: 60)
+    turtle.forward(steps: 3 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     fillSquare()
-    turtle.forward(steps: 40)
+    turtle.forward(steps: 2 * scale)
     fillSquare()
     fillSquare()
-    
+
     //draw row 3
     turtle.left(by: 180)
-    turtle.forward(steps: 100)
+    turtle.forward(steps: 5 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     fillSquare()
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     fillSquare()
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     fillSquare()
-    
+
     //draw row 4
     turtle.left(by: 180)
-    turtle.forward(steps: 100)
+    turtle.forward(steps: 5 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     fillSquare()
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     fillSquare()
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     fillSquare()
-    
+
     //draw row 5
     turtle.left(by: 180)
-    turtle.forward(steps: 120)
+    turtle.forward(steps: 6 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     fillSquare()
     fillSquare()
-    turtle.forward(steps: 40)
+    turtle.forward(steps: 2 * scale)
     fillSquare()
-    
+
     //draw row 6
     turtle.left(by: 180)
-    turtle.forward(steps: 120)
+    turtle.forward(steps: 6 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 20)
+    turtle.forward(steps: 1 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 60)
+    turtle.forward(steps: 3 * scale)
     fillSquare()
     fillSquare()
     fillSquare()
-    
+
     //go to position to start another tessellation
     turtle.right(by: 90)
-    turtle.forward(steps: 100)
+    turtle.forward(steps: 5 * scale)
     turtle.left(by: 90)
     //turtle.drawSelf()
-    
+
 }
 
 for _ in 1 ...  5 {
 
     for _ in 1 ... 5 {
-        
+
         drawTessellation()
-        
+
     }
-    
+
     //Go to start of next row
     turtle.left(by: 180)
-    turtle.forward(steps: 600)
+    turtle.forward(steps: 30 * scale)
     turtle.right(by: 90)
-    turtle.forward(steps: 120)
+    turtle.forward(steps: 6 * scale)
     turtle.right(by: 90)
-    
+
 }
     
 canvas.highPerformance = false
